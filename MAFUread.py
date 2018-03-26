7e0650525281db450e5d0fb2884d9dc4b279dace
MafuMafu2 <34209866+MafuMafu2@users.noreply.github.com>
2018-03-26 19:11:44 +0800

    Add files via upload

---
+++ MAFUread.py
@@ -0,0 +1,406 @@
+# -*- coding: utf-8 -*-
+
+from linepy import *
+from datetime import datetime
+from time import sleep
+from bs4 import BeautifulSoup
+from humanfriendly import format_timespan, format_size, format_number, format_length
+import time, random, sys, json, codecs, threading, glob, re, string, os, requests, subprocess, six, ast, pytz, urllib, urllib.parse
+from gtts import gTTS
+from googletrans import Translator
+#==============================================================================#
+botStart = time.time()
+
+nadya = LINE()
+#nadya = LINE("ErwQwDYT4QGCeHIVOWdd./hzYkv7U8XdgFvZOg78yNq.cWyyAoQ+JyCvBftJVP/GZZL1fWnwem4C9PA6V36cX8I=")
+#nadya = LINE("Email","Password")
+nadya.log("Auth Token : " + str(nadya.authToken)) 
+channelToken = nadya.getChannelResult()
+nadya.log("Channel Token : " + str(channelToken))
+
+nadyaMID = nadya.profile.mid
+nadyaProfile = nadya.getProfile()
+lineSettings = nadya.getSettings()
+oepoll = OEPoll(nadya)
+wait2 = {
+    'readPoint':{},
+    'readMember':{},
+    'setTime':{},
+    'ROM':{}
+}
+setTime = {}
+setTime = wait2['setTime']
+msg_dict = {}
+bl = [""]
+#==============================================================================#
+readOpen = codecs.open("read.json","r","utf-8")
+settingsOpen = codecs.open("temp.json","r","utf-8")
+
+read = json.load(readOpen)
+settings = json.load(settingsOpen)
+
+
+myProfile = {
+	"displayName": "",
+	"statusMessage": "",
+	"pictureStatus": ""
+}
+
+myProfile["displayName"] = nadyaProfile.displayName
+myProfile["statusMessage"] = nadyaProfile.statusMessage
+myProfile["pictureStatus"] = nadyaProfile.pictureStatus
+#==============================================================================#
+def restartBot():
+    print ("[ INFO ] BOT RESETTED")
+    backupData()
+#    time.sleep(3)
+    python = sys.executable
+    os.execl(python, python, *sys.argv)
+    
+def backupData():
+    try:
+        backup = settings
+        f = codecs.open('temp.json','w','utf-8')
+        json.dump(backup, f, sort_keys=True, indent=4, ensure_ascii=False)
+        backup = read
+        f = codecs.open('read.json','w','utf-8')
+        json.dump(backup, f, sort_keys=True, indent=4, ensure_ascii=False)
+        return True
+    except Exception as error:
+        logError(error)
+        return False    
+    
+def logError(text):
+    nadya.log("[ ERROR ] " + str(text))
+    time_ = datetime.now()
+    with open("errorLog.txt","a") as error:
+        error.write("\n[%s] %s" % (str(time), text))
+        
+def sendMessageWithMention(to, mid):
+    try:
+        aa = '{"S":"0","E":"3","M":'+json.dumps(mid)+'}'
+        text_ = '@x '
+        nadya.sendMessage(to, text_, contentMetadata={'MENTION':'{"MENTIONEES":['+aa+']}'}, contentType=0)
+    except Exception as error:
+        logError(error)
+        
+def helpmessage():
+    helpMessage = "╔═══════════════" + "\n" + \
+                  "╠ MAFU BOT" + "\n" + \
+                  "╠選單列表════════" + "\n" + \
+                  "╠➥ mafu 指令表" + "\n" + \
+                  "╠➥ Me 查看個人資料" + "\n" + \
+                  "╠➥ Tagall 全體標註" + "\n" + \
+                  "╠➥ Read On/Off/Reset 查看已讀開關" + "\n" + \
+                  "╠➥ Read 查看已讀" + "\n" + \
+				  "╠➥ reread on/off
+                  "║" + "\n" + \
+                  "╚═〘Make By Mafu〙"
+    return helpMessage
+#==============================================================================#
+def lineBot(op):
+    try:
+        if op.type == 0:
+            print ("[ 0 ] END OF OPERATION")
+            return
+        if op.type == 5:
+            print ("[ 5 ] NOTIFIED ADD CONTACT")
+            if settings["autoAdd"] == True:
+                nadya.sendMessage(op.param1, "Halo {} terimakasih telah menambahkan saya sebagai teman :D".format(str(nadya.getContact(op.param1).displayName)))
+        if op.type == 13:
+            print ("[ 13 ] NOTIFIED INVITE GROUP")
+            group = nadya.getGroup(op.param1)
+            if settings["autoJoin"] == True:
+                nadya.acceptGroupInvitation(op.param1)
+        if op.type == 24:
+            print ("[ 24 ] NOTIFIED LEAVE ROOM")
+            if settings["autoLeave"] == True:
+                nadya.leaveRoom(op.param1)
+        if op.type == 26:
+            try:
+                msg = op.message
+                if settings["reread"] == True:
+                    if msg.toType == 0:
+                        nadya.log("[%s]"%(msg._from)+msg.text)
+                    else:
+                        nadya.log("[%s]"%(msg.to)+msg.text)
+                    if msg.contentType == 0:
+                        msg_dict[msg.id] = {"text":msg.text,"from":msg._from,"createdTime":msg.createdTime}
+                else:
+                    pass
+            except Exception as e:
+                print(e)
+        if op.type == 65:
+            try:
+                at = op.param1
+                msg_id = op.param2
+                if settings["reread"] == True:
+                    if msg_id in msg_dict:
+                        if msg_dict[msg_id]["from"] not in bl:
+                            nadya.sendMessage(at,"[收回訊息者]\n%s\n[訊息內容]\n%s"%(nadya.getContact(msg_dict[msg_id]["from"]).displayName,msg_dict[msg_id]["text"]))
+                        del msg_dict[msg_id]
+                else:
+                    pass
+            except Exception as e:
+                print(e)
+        if op.type == 25:
+            print ("[ 25 ] SEND MESSAGE")
+            msg = op.message
+            text = msg.text
+            msg_id = msg.id
+            receiver = msg.to
+            sender = msg._from
+            if msg.toType == 0:
+                if sender != nadya.profile.mid:
+                    to = sender
+                else:
+                    to = receiver
+            else:
+                to = receiver
+            if msg.contentType == 0:
+                if text is None:
+                    return
+               
+#==============================================================================#
+ 
+#==============================================================================#
+                elif text.lower() == 'speed':
+                    start = time.time()
+                    nadya.sendMessage(to, "檢查中...")
+                    elapsed_time = time.time() - start
+                    nadya.sendMessage(to,format(str(elapsed_time)))
+                elif text.lower() == 'restart':
+                    nadya.sendMessage(to, "重啟中")
+                    time.sleep(5)
+                    nadya.sendMessage(to, "重啟完成")
+                    restartBot()
+#==============================================================================#
+
+#==============================================================================#
+                elif text.lower() == 'reread on':
+                    settings["reread"] = True
+                    nadya.sendMessage(to, "查詢收回開啟")
+                elif text.lower() == 'reread off':
+                    settings["reread"] = False
+                    nadya.sendMessage(to, "查詢收回關閉")
+                elif text.lower() == 'me':
+                    sendMessageWithMention(to, nadyaMID)
+                    nadya.sendContact(to, nadyaMID)
+#==============================================================================#
+ 
+#==============================================================================#
+  
+#==============================================================================#          
+                elif text.lower() == 'tagall':
+                    group = nadya.getGroup(msg.to)
+                    nama = [contact.mid for contact in group.members]
+                    k = len(nama)//100
+                    for a in range(k+1):
+                        txt = u''
+                        s=0
+                        b=[]
+                        for i in group.members[a*100 : (a+1)*100]:
+                            b.append({"S":str(s), "E" :str(s+6), "M":i.mid})
+                            s += 7
+                            txt += u'@Alin \n'
+                        nadya.sendMessage(to, text=txt, contentMetadata={u'MENTION': json.dumps({'MENTIONEES':b})}, contentType=0)
+                        nadya.sendMessage(to, "已標註 {} 人 ".format(str(len(nama))))          
+                elif text.lower() == 'read on':
+                    tz = pytz.timezone("Asia/Jakarta")
+                    timeNow = datetime.now(tz=tz)
+                    day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday","Friday", "Saturday"]
+                    hari = ["週日", "週一", "週二", "週三", "周四", "週五", "週六"]
+                    bulan = ["Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember"]
+                    hr = timeNow.strftime("%A")
+                    bln = timeNow.strftime("%m")
+                    for i in range(len(day)):
+                        if hr == day[i]: hasil = hari[i]
+                    for k in range(0, len(bulan)):
+                        if bln == str(k): bln = bulan[k-1]
+                    readTime = hasil + ", " + timeNow.strftime('%Y') + " - " + bln + " - " + timeNow.strftime('%d') + "\n[ " + timeNow.strftime('%H:%M:%S') + " ]"
+                    if msg.to in read['readPoint']:
+                            try:
+                                del read['readPoint'][msg.to]
+                                del read['readMember'][msg.to]
+                                del read['readTime'][msg.to]
+                            except:
+                                pass
+                            read['readPoint'][msg.to] = msg.id
+                            read['readMember'][msg.to] = ""
+                            read['readTime'][msg.to] = datetime.now().strftime('%H:%M:%S')
+                            read['ROM'][msg.to] = {}
+                            with open('read.json', 'w') as fp:
+                                json.dump(read, fp, sort_keys=True, indent=4)
+                                nadya.sendMessage(msg.to,"已讀已開啟")
+                    else:
+                        try:
+                            del read['readPoint'][msg.to]
+                            del read['readMember'][msg.to]
+                            del read['readTime'][msg.to]
+                        except:
+                            pass
+                        read['readPoint'][msg.to] = msg.id
+                        read['readMember'][msg.to] = ""
+                        read['readTime'][msg.to] = datetime.now().strftime('%H:%M:%S')
+                        read['ROM'][msg.to] = {}
+                        with open('read.json', 'w') as fp:
+                            json.dump(read, fp, sort_keys=True, indent=4)
+                            nadya.sendMessage(msg.to, "已設立已讀點:\n" + readTime)
+                            
+                elif text.lower() == 'read off':
+                    tz = pytz.timezone("Asia/Jakarta")
+                    timeNow = datetime.now(tz=tz)
+                    day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday","Friday", "Saturday"]
+                    hari = ["週日", "週一", "週二", "週三", "周四", "週五", "週六"]
+                    bulan = ["Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember"]
+                    hr = timeNow.strftime("%A")
+                    bln = timeNow.strftime("%m")
+                    for i in range(len(day)):
+                        if hr == day[i]: hasil = hari[i]
+                    for k in range(0, len(bulan)):
+                        if bln == str(k): bln = bulan[k-1]
+                    readTime = hasil + ", " + timeNow.strftime('%Y') + " - " + bln + " - " + timeNow.strftime('%d') + "\n[ " + timeNow.strftime('%H:%M:%S') + " ]"
+                    if msg.to not in read['readPoint']:
+                        nadya.sendMessage(msg.to,"已讀已關閉")
+                    else:
+                        try:
+                            del read['readPoint'][msg.to]
+                            del read['readMember'][msg.to]
+                            del read['readTime'][msg.to]
+                        except:
+                              pass
+                        nadya.sendMessage(msg.to, "已刪除已讀點\n" + readTime)
+    
+                elif text.lower() == 'read reset':
+                    tz = pytz.timezone("Asia/Jakarta")
+                    timeNow = datetime.now(tz=tz)
+                    day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday","Friday", "Saturday"]
+                    hari = ["週日", "週一", "週二", "週三", "周四", "週五", "週六"]
+                    bulan = ["Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember"]
+                    hr = timeNow.strftime("%A")
+                    bln = timeNow.strftime("%m")
+                    for i in range(len(day)):
+                        if hr == day[i]: hasil = hari[i]
+                    for k in range(0, len(bulan)):
+                        if bln == str(k): bln = bulan[k-1]
+                    readTime = hasil + ", " + timeNow.strftime('%Y') + " - " + bln + " - " + timeNow.strftime('%d') + "\n[ " + timeNow.strftime('%H:%M:%S') + " ]"
+                    if msg.to in read["readPoint"]:
+                        try:
+                            del read["readPoint"][msg.to]
+                            del read["readMember"][msg.to]
+                            del read["readTime"][msg.to]
+                        except:
+                            pass
+                        nadya.sendMessage(msg.to, "已重製已讀點\n" + readTime)
+                    else:
+                        nadya.sendMessage(msg.to, "尚無已讀點")
+                        
+                elif text.lower() == 'read':
+                    tz = pytz.timezone("Asia/Jakarta")
+                    timeNow = datetime.now(tz=tz)
+                    day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday","Friday", "Saturday"]
+                    hari = ["週日", "週一", "週二", "週三", "周四", "週五", "週六"]
+                    bulan = ["Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember"]
+                    hr = timeNow.strftime("%A")
+                    bln = timeNow.strftime("%m")
+                    for i in range(len(day)):
+                        if hr == day[i]: hasil = hari[i]
+                    for k in range(0, len(bulan)):
+                        if bln == str(k): bln = bulan[k-1]
+                    readTime = hasil + ", " + timeNow.strftime('%Y') + " - " + bln + " - " + timeNow.strftime('%d') + "\n[ " + timeNow.strftime('%H:%M:%S') + " ]"
+                    if receiver in read['readPoint']:
+                        if read["ROM"][receiver].items() == []:
+                            nadya.sendMessage(receiver,"[ Reader ]:\nNone")
+                        else:
+                            chiya = []
+                            for rom in read["ROM"][receiver].items():
+                                chiya.append(rom[1])
+                            cmem = nadya.getContacts(chiya) 
+                            zx = ""
+                            zxc = ""
+                            zx2 = []
+                            xpesan = '[ 已讀者 ]:\n'
+                        for x in range(len(cmem)):
+                            xname = str(cmem[x].displayName)
+                            pesan = ''
+                            pesan2 = pesan+"@c\n"
+                            xlen = str(len(zxc)+len(xpesan))
+                            xlen2 = str(len(zxc)+len(pesan2)+len(xpesan)-1)
+                            zx = {'S':xlen, 'E':xlen2, 'M':cmem[x].mid}
+                            zx2.append(zx)
+                            zxc += pesan2
+                        text = xpesan+ zxc + "\n[ 已讀點設立時間 ]: \n" + readTime
+                        try:
+                            nadya.sendMessage(receiver, text, contentMetadata={'MENTION':str('{"MENTIONEES":'+json.dumps(zx2).replace(' ','')+'}')}, contentType=0)
+                        except Exception as error:
+                            print (error)
+                        pass
+                    else:
+                        nadya.sendMessage(receiver,"請先設立已讀點")
+#==============================================================================#
+ 
+                    
+#==============================================================================#
+        if op.type == 26:
+            print ("[ 26 ] RECEIVE MESSAGE")
+            msg = op.message
+            text = msg.text
+            msg_id = msg.id
+            receiver = msg.to
+            sender = msg._from
+            if msg.toType == 0:
+                if sender != nadya.profile.mid:
+                    to = sender
+                else:
+                    to = receiver
+            else:
+                to = receiver
+                if settings["autoRead"] == True:
+                    nadya.sendChatChecked(to, msg_id)
+                if to in read["readPoint"]:
+                    if sender not in read["ROM"][to]:
+                        read["ROM"][to][sender] = True
+                if sender in settings["mimic"]["target"] and settings["mimic"]["status"] == True and settings["mimic"]["target"][sender] == True:
+                    text = msg.text
+                    if text is not None:
+                        nadya.sendMessage(msg.to,text)
+                if msg.contentType == 0 and sender not in nadyaMID and msg.toType == 2:
+                    if 'MENTION' in msg.contentMetadata.keys()!= None:
+                        names = re.findall(r'@(\w+)', text)
+                        mention = ast.literal_eval(msg.contentMetadata['MENTION'])
+                        mentionees = mention['MENTIONEES']
+                        lists = []
+                        for mention in mentionees:
+                            if nadyaMID in mention["M"]:
+                                if settings["detectMention"] == True:
+                                    contact = nadya.getContact(sender)
+                                    nadya.sendMessage(to, "sundala nu")
+                                    sendMessageWithMention(to, contact.mid)
+                                break
+#==============================================================================#
+        if op.type == 55:
+            print ("[ 55 ] NOTIFIED READ MESSAGE")
+            try:
+                if op.param1 in read['readPoint']:
+                    if op.param2 in read['readMember'][op.param1]:
+                        pass
+                    else:
+                        read['readMember'][op.param1] += op.param2
+                    read['ROM'][op.param1][op.param2] = op.param2
+                    backupData()
+                else:
+                   pass
+            except:
+                pass
+    except Exception as error:
+        logError(error)
+#==============================================================================#
+while True:
+    try:
+        ops = oepoll.singleTrace(count=50)
+        if ops is not None:
+            for op in ops:
+                lineBot(op)
+                oepoll.setRevision(op.revision)
+    except Exception as e:
+        logError(e)
